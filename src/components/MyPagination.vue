310c1abf8dc067df4d05f6642ef37f10c4dfa3a4
chenzhiguo <1090236106@qq.com>
2022-07-27 23:47:24 +0800

    完成分页功能

---
+++ src/components/MyPagination.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="block">
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page.sync="currentPage1"
+      :page-size="100"
+      layout="prev, pager, next, jumper"
+      :total="1000"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.currentPage1 = val;
+      this.$emit("changePage", val);
+    },
+  },
+  data() {
+    return {
+      currentPage1: 1,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.block {
+  text-align: center;
+  margin: 30px;
+}
+</style>
--- src/views/Home/Lanmei.vue
+++ src/views/Home/Lanmei.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>
-    <h2>蓝莓页面</h2>
-    <div v-for="item in list" :key="item.index">
-      <img :src="item" alt="" />
-    </div>
+  <div class="container">
+    <h2 class="title">蓝莓酱</h2>
+    <ul class="list">
+      <li v-for="(item, index) in list" :key="index">
+        <div class="item">
+          <img :src="item.img" :alt="item.title" />
+          <span>{{ item.title }}</span>
+        </div>
+      </li>
+      <div class="clear"></div>
+    </ul>
   </div>
 </template>
 
@@ -21,9 +27,37 @@ export default {
 </script>
 
 <style lang="less" scoped>
-h2 {
-  width: 100px;
-  height: 30px;
-  margin: 30px auto;
+.container {
+  width: 1200px;
+  margin: 20px auto;
+}
+.title {
+  text-align: center;
+  padding: 10px;
+}
+
+.item {
+  position: relative;
+  span {
+    position: absolute;
+    right: 36px;
+    bottom: 16px;
+    border-radius: 4px;
+    color: #fff;
+    background: rgba(0, 0, 0, 0.2);
+  }
+  img {
+    padding-right: 30px;
+    padding-bottom: 10px;
+  }
+}
+
+.list {
+  li {
+    float: left;
+  }
+}
+.clear {
+  clear: both;
 }
 </style>
