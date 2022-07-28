02654be8cafc766967fa36851c8bfc5c62e2fad0
chenzhiguo <1090236106@qq.com>
2022-07-29 00:14:10 +0800

    新闻页面要闻内容

--- src/views/News.vue
+++ src/views/News.vue
@@ -2,14 +2,21 @@
   <div class="container">
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="要闻" name="first">
-        <ul class="list" v-for="(item, index) in yaowenList" :key="index">
-          <li>
+        <ul class="list">
+          <li v-for="(item, index) in yaowenList" :key="index">
             <img :src="item.imgsrc" alt="" />
-            <span>{{ item.title }}</span>
-            <div>{{ item.title }}</div>
+            <span class="title"
+              ><a :href="item.url">{{ item.title }}</a></span
+            >
+            <div class="content">
+              <span>来源：{{ item.source }}</span>
+              <span>时间：{{ item.ptime }}</span>
+              <span>评论数: {{ item.commentCount }}</span>
+            </div>
             <div class="clear"></div>
           </li>
         </ul>
+        <MyPagination @changePage="changePage"></MyPagination>
       </el-tab-pane>
       <el-tab-pane label="国内" name="second">国内</el-tab-pane>
     </el-tabs>
@@ -18,6 +25,7 @@
 
 <script>
 import { yaowen, guonei } from "../api/type";
+import MyPagination from "../components/MyPagination.vue";
 export default {
   data() {
     return {
@@ -43,11 +51,34 @@ export default {
     handleClick(tab, event) {
       console.log(tab, event);
     },
+    // 接收当前页码
+    changePage(val) {
+      this.$api.getNews(yaowen, val).then((res) => {
+        console.log("要闻数据", res);
+        this.yaowenList = res;
+      });
+      document.documentElement.scrollTop = 0;
+    },
+  },
+  components: {
+    MyPagination,
   },
 };
 </script>
 
 <style lang="less" scoped>
+a:link {
+  text-decoration: none;
+  color: #404040;
+}
+a:active {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+  color: #404040;
+}
 .container {
   margin: 5px auto;
   width: 1200px;
@@ -58,15 +89,28 @@ img {
   width: 140px;
   height: 88px;
   margin-right: 10px;
-  margin-bottom: 10px;
 }
 
 .clear {
   clear: both;
 }
 
-span {
+.title {
   color: #404040;
   font-size: 20px;
+}
+
+.content {
+  margin-top: 43.2px;
+  font-size: 12px;
+  span {
+    margin-right: 20px;
+    color: #888;
+  }
+}
+
+li {
+  border: 1px solid orange;
+  margin-bottom: 10px;
 }
 </style>
